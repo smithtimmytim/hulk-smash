e2f83890e549f9e1f9fa6797656981ad8089a7a8
Timothy B. Smith <smith@ttimsmith.com>
2019-06-02 22:25:35 -0700

    Cache busting this bitch

--- functions.php
+++ functions.php
@@ -7,19 +7,19 @@
 add_theme_support( 'post-thumbnails' ); 
 
 function _themename_assets() {
-  wp_enqueue_style( '_themename-stylesheet', get_template_directory_uri() . '/dist/css/hulk-smash.css', array(), '1.0.0', 'all' );
+  wp_enqueue_style( '_themename-stylesheet', get_template_directory_uri() . '/dist/css/hulk-smash.css', array(), '2019.1', 'all' );
 
-  wp_enqueue_script( '_themename-scripts', get_template_directory_uri() . '/dist/js/hulk-smash.js', array(), '1.0.0', true );
+  wp_enqueue_script( '_themename-scripts', get_template_directory_uri() . '/dist/js/hulk-smash.js', array(), '2019.1', true );
 }
 add_action('wp_enqueue_scripts', '_themename_assets');
 
 function wpdocs_excerpt_more( $more ) {
-    return '…<a href="'.get_the_permalink().'" class="post-list__more" rel="nofollow"><span class="caps">More</span></a>';
+  return '…<a href="'.get_the_permalink().'" class="post-list__more" rel="nofollow"><span class="caps">More</span></a>';
 }
 add_filter( 'excerpt_more', 'wpdocs_excerpt_more' );
 
 function wpdocs_custom_excerpt_length( $length ) {
-    return 30;
+  return 30;
 }
 add_filter( 'excerpt_length', 'wpdocs_custom_excerpt_length', 999 );
 
