1fcf939b25ddf35fc18432d00550b6ba1da5607b
Timothy B. Smith <smith@ttimsmith.com>
2019-06-06 21:53:43 -0700

    Add jump links to archive

--- page-archive.php
+++ page-archive.php
@@ -13,6 +13,31 @@ get_header(); ?>
     </div>
   </header>
 
+  <ul class="groups-list groups-list--3-col">
+    <?php
+      query_posts(array('nopaging' => 1, /* we want all posts, so disable paging. Order by date is default */));
+      $prev_year = null;
+      if ( have_posts() ) {
+        while ( have_posts() ) {
+            the_post();
+            $this_year = get_the_date('Y');
+            if ($prev_year != $this_year) {
+                  // Year boundary
+                  if (!is_null($prev_year)) {
+                    // A list is already open, close it first
+                    echo '</li>';
+                  }
+                  echo '<li><a href="#'. $this_year .'">'. $this_year .'</a>';
+              }
+              
+              $prev_year = $this_year;
+          }
+          echo '</li>';
+      }
+    ?>
+
+  </ul>
+
   <?php
     query_posts(array('nopaging' => 1, /* we want all posts, so disable paging. Order by date is default */));
     $prev_year = null;
@@ -24,9 +49,9 @@ get_header(); ?>
               // Year boundary
               if (!is_null($prev_year)) {
                 // A list is already open, close it first
-                echo '</ul></section>';
+                echo '</ul><a href="#page-title" class="group__return-home">Back to Top &uarr;</a></section>';
               }
-              echo '<section class="group-section"><h2 class="group__name">' . $this_year . '</h2><ul class="group__post-list group__post-list--2-col">';
+              echo '<section class="group-section" id="' . $this_year . '"><h2 class="group__name">' . $this_year . '</h2><ul class="group__post-list group__post-list--2-col">';
           }
           echo '<li class="group-post__item group-post__item--bold"><a href="'. get_permalink() .'">' . get_the_title();
           if (get_field('rating')) {
@@ -36,7 +61,7 @@ get_header(); ?>
           echo '</a><span class="group-post__meta-info">' . get_the_date('M d') . '</li>';
           $prev_year = $this_year;
       }
-      echo '</ul></section>';
+      echo '</ul><a href="#page-title" class="group__return-home">Back to Top &uarr;</a></section>';
     }
   ?>
 
