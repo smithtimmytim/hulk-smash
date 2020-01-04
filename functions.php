7b4cbff4d1293c29c4b4f323348a8f7eac35dee7
Timothy B. Smith <smith@ttimsmith.com>
2020-01-03 20:46:53 -0800

    Update version number

--- functions.php
+++ functions.php
@@ -7,9 +7,9 @@
 add_theme_support( 'post-thumbnails' ); 
 
 function _themename_assets() {
-  wp_enqueue_style( '_themename-stylesheet', get_template_directory_uri() . '/dist/css/hulk-smash.css', array(), '2019.15', 'all' );
+  wp_enqueue_style( '_themename-stylesheet', get_template_directory_uri() . '/dist/css/hulk-smash.css', array(), '2020.001', 'all' );
 
-  wp_enqueue_script( '_themename-scripts', get_template_directory_uri() . '/dist/js/hulk-smash.js', array(), '2019.15', true );
+  wp_enqueue_script( '_themename-scripts', get_template_directory_uri() . '/dist/js/hulk-smash.js', array(), '2020.001', true );
 }
 add_action('wp_enqueue_scripts', '_themename_assets');
 
